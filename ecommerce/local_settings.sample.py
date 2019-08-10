cfb2f87dcb25871f9c178fd48fe5c86699a0296b
SanipRijal <saneprijal@gmail.com>
2019-08-10 15:54:28 +0545

    manage static root

--- ecommerce/local_settings.sample.py
+++ ecommerce/local_settings.sample.py
@@ -26,7 +26,4 @@ DATABASES = {
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Application definition
 
 INSTALLED_APPS = [
